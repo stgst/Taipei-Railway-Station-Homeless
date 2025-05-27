d2b7af3ec862f69d4da5805e29f842ec76b26919
xiung <steven19790906@gmail.com>
2025-05-27 16:50:23 +0800

    20250527: First initalize

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -2,13 +2,12 @@
 
     <div id="title" class="leading-15 text-white">
         <div class="text-left">
-            <span class="text-5xl ml-10"></span><br>
-        </div>
-        <div class="text-right">
-            <span class="text-7xl text-center me-15 text-violet-200">無家者</span><br>
-
-            <div class="text-right"></div>
-            <span class="text-5xl mr-10">嗎？</span>
+            <span class="text-5xl ml-10">　　我們曾經到無家者議題倡議組織「人生百味」進行田野調查，也陸續參與了人生百味所辦理的街頭尾牙、芒草心所辦理的無家者真人圖書館。而這些經歷使我們對於無家者議題有許多新的認識，也使我們期望能夠向大眾分享我們在無家者議題中看見的、無家者們在自己的立場中想說的。
+　　為此，我們籌備了這次的北車導覽活動，預計費時90分鐘，跟著無家者 OA 大哥的腳步，穿越了台北車站周邊的街道、走進了他的記憶與生活。
+　　最後，我們選了一段詩作來作結，是周盈秀的〈我姐姐住台北〉：
+「冬天的夜晚，姐姐會摘下弦月，輕輕刮除整座城市寂寞的細節。」
+　　我們認為，這句話提醒我們，其實每個人，不管住在哪裡、過著什麼樣的生活，都在用自己的方式對抗城市裡的孤單與冷漠。而我們的活動，就是想讓更多人看見：在城市邊緣生活的那些人，也有夢想、有故事、有被理解的需求。
+　　我們希望，這不是結束，而是一段理解與行動的開始。</span>
         </div>
     </div>
     <div id="icon" class="text-left mt-10">
---
+++ vue.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    publicPath: '/Taipei-Railway-Station-Homeless/dist/',
+}
