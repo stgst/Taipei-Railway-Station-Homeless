c5ef89b1f512d70885c6b17796797ae77a66aa2e
xiung <steven19790906@gmail.com>
2025-05-29 15:45:51 +0800

    20250529: 新增返回鍵

--- src/components/Btn.vue
+++ src/components/Btn.vue
@@ -3,18 +3,25 @@ import { inject } from 'vue'
 
 const pageID = inject('pageID')
 
-const handleClick = () => {
+const handleClickPLUS = () => {
   pageID.value++
 }
+const handleClickSUB = () => {
+  pageID.value--
+  if (pageID.value < 0) {
+    pageID.value = 0 // 確保不會小於0
+  }
+}
+
 </script>
 
 <template>
   <div id="btn" class="flex items-center justify-center">
-    <button 
-      class="cursor-pointer animate__animated animate__pulse animate__infinite"
-      @click="handleClick"
-    >
-      <img src="@/assets/img/btn.png" class=" w-15 h-15 md:w-12 md:h-12 border-2 border-white rounded-full">
+    <button class="cursor-pointer mx-5 animate__animated animate__pulse animate__infinite" @click="handleClickSUB">
+      <img src="@/assets/img/btn.png" class=" w-15 h-15 md:w-12 md:h-12 border-2 border-white rounded-full rotate-90">
+    </button>
+    <button class="cursor-pointer mx-5 animate__animated animate__pulse animate__infinite" @click="handleClickPLUS">
+      <img src="@/assets/img/btn.png" class=" w-15 h-15 md:w-12 md:h-12 border-2 border-white rounded-full rotate-270">
     </button>
   </div>
 </template>
